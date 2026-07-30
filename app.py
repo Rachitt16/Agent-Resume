9b120b30c17e9e4d6ec3c5a29df678a741245866
Rachitt16 <rachitjhanb16@gmail.com>
2026-07-30 10:31:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ resume with latest job apply links""")
 
 st.image("bg.png")
 
+st.sidebar.title("Fill Important Data")
+st.sidebar.image("bg.png")
+
 GOOGLE_API_KEY = "AQ.Ab8RN6Kjesilv7EvdFQcf-FWTguHfY47ia38gtGgwEhM3X28mg"
 GROQ_API_KEY = "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"
 TAVILY_API_KEY = "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"
